e2a17b62b823964d805b5b2a6e57687d8340fe15
Kirill Soloshenko <smertlove@yandex.ru>
2021-09-09 01:50:38 +0300

    умеет вертеть карточку и обновлять на ней слова

---
+++ heiroglyphs.js
@@ -0,0 +1,97 @@
+var heiroglyphs =	[{
+    "hieroglyph": "你",
+    "translation": "ты",
+    "pinyin": "Nǐ",
+    "lesson": 1
+},
+{
+    "hieroglyph": "人",
+    "translation": "человек",
+    "pinyin": "Rén",
+    "lesson": 1
+},
+{
+    "hieroglyph": "女",
+    "translation": "женщина",
+    "pinyin": "Nǚ",
+    "lesson": 1
+},
+{
+    "hieroglyph": "子",
+    "translation": "ребенок",
+    "pinyin": "Zǐ",
+    "lesson": 1
+},
+{
+    "hieroglyph": "好",
+    "translation": "хорошо, хороший",
+    "pinyin": "Hǎo",
+    "lesson": 1
+},
+{
+    "hieroglyph": "口",
+    "translation": "рот",
+    "pinyin": "Kǒu",
+    "lesson": 2
+},
+{
+    "hieroglyph": "马",
+    "translation": "лошадь",
+    "pinyin": "Mǎ",
+    "lesson": 2
+},
+{
+    "hieroglyph": "吗",
+    "translation": "(вопросит. частица)",
+    "pinyin": "Мa",
+    "lesson": 2
+},
+{
+    "hieroglyph": "我",
+    "translation": "я",
+    "pinyin": "Wǒ",
+    "lesson": 2
+},
+{
+    "hieroglyph": "很",
+    "translation": "очень",
+    "pinyin": "Hěn",
+    "lesson": 2
+},
+{
+    "hieroglyph": "艮",
+    "translation": "твердый",
+    "pinyin": "Gěn",
+    "lesson": 2
+},
+{
+    "hieroglyph": "彳",
+    "translation": "шаг",
+    "pinyin": "Chì",
+    "lesson": 2
+},
+{
+    "hieroglyph": "呢",
+    "translation": "(модальная частица)",
+    "pinyin": "Ne",
+    "lesson": 2
+},
+{
+    "hieroglyph": "尸",
+    "translation": "труп",
+    "pinyin": "Shī",
+    "lesson": 2
+},
+{
+    "hieroglyph": "匕",
+    "translation": "ложка, черпак",
+    "pinyin": "Bǐ",
+    "lesson": 2
+},
+{
+    "hieroglyph": "也",
+    "translation": "тоже, также, и",
+    "pinyin": "Yě",
+    "lesson": 2
+}
+]
---
+++ script.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var front = document.getElementById("frnt");
+var back = document.getElementById("bck");
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+  
+function getExercise(hieroglyphs){
+    let entry = hieroglyphs[getRandomInt(0, hieroglyphs.length)];
+    return entry;
+}
+
+
+function change(){   
+    let entry = getExercise(heiroglyphs);
+    front.textContent = entry.translation;
+    back.textContent = entry.hieroglyph + ' ' + entry.pinyin;
+}
+
+change()
